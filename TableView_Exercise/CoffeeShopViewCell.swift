23089198eadfbd68442a8bee946ce4ff1980827c
Bayu Yasaputro <bayuwy@gmail.com>
2022-09-20 21:55:22 +0700

    Initial commit

---
+++ TableView_Exercise/CoffeeShopViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CoffeeShopViewCell.swift
+//  TableView_Exercise
+//
+//  Created by Bayu Yasaputro on 16/09/22.
+//
+
+import UIKit
+
+class CoffeeShopViewCell: UITableViewCell {
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbImageView.layer.cornerRadius = 16
+        thumbImageView.layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableView_Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Exercise
+//
+//  Created by Bayu Yasaputro on 16/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
